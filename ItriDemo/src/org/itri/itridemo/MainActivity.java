a663b2229cd3ea4076b270c02e8eff9342e12285
Mark Ho <mark@patristar.com>
2013-02-22 15:03:24 +0800

    Modify something again

--- ItriDemo/src/org/itri/itridemo/MainActivity.java
+++ ItriDemo/src/org/itri/itridemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,27 @@ public class MainActivity extends Activity {
 
 
     @Override
+	public void addContentView(View view, LayoutParams params) {
+		// TODO Auto-generated method stub
+		super.addContentView(view, params);
+	}
+
+
+	@Override
+	public void closeContextMenu() {
+		// TODO Auto-generated method stub
+		super.closeContextMenu();
+	}
+
+
+	@Override
+	public void closeOptionsMenu() {
+		// TODO Auto-generated method stub
+		super.closeOptionsMenu();
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
